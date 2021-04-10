6b41cefed0b8864729d72669016ae3fb1ea9099b
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-10 15:18:22 -0700

    basework added

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var startBtnEl = document.querySelector("#startBtn");
+var questionSecEl = document.querySelector("#questionSec");
+var questionsEl = document.querySelector("#questions");
+var answerListEl = document.querySelector("#slct");
+var navEl = document.querySelector(".navBar");
+
+var question1Arr = ["sis", "whu", "oijads", "uoaushd"]
+
+var startBtnEl = function(event) {
+    
+    var question = document.createElement("div");
+    question.innerHTML = "<h2>Can you see the rain?</h2>";
+    questionSecEl.appendChild(question);
+
+    for (var i = 0; question1Arr.length; i++) {
+        createElement.
+    }
+};
+   
+
+
+navEl.addEventListener("click", startBtnEl);
